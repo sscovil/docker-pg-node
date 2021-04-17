b0513ec6a33ba6cad769be12577117998064ec01
Shaun Scovil <sscovil@Shauns-MacBook-Air.local>
2021-04-17 15:28:47 -0400

    Add clustering to utilize multiple CPU cores

--- src/server.js
+++ src/server.js
@@ -2,26 +2,60 @@ const { api: config } = require('./config');
 const pool = require('./database');
 const router = require('./router');
 const Migrations = require('./lib/migrations');
+
 const http = require('http');
+const os = require("os")
+const cluster = require("cluster")
 
-const server = http.createServer(router);
+async function createCluster() {
+    const cpuCount = os.cpus().length;
 
-server.on('clientError', (err, socket) => {
-    socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
-});
+    if (cpuCount > 1) {
+        if (cluster.isMaster) {
+            for (let i = 0; i < cpuCount; i++) {
+                cluster.fork();
+            }
+            cluster.on('exit', worker => {
+                console.log(`Worker ${worker.id} has exited`);
+            });
+        } else {
+            await createServer();
+        }
+    } else {
+        await createServer();
+    }
+}
 
-server.on('listening', () => {
-    const { host, port } = config;
-    console.log(`Server listening at http://${host}:${port}`);
-});
+async function createServer() {
+    const server = http.createServer(router);
 
-(async () => {
-    const db = await pool.connect();
-    console.log('Created connection pool for PostgreSQL database');
-    const migrations = new Migrations(db);
-    await migrations.run();
-    server.db = db;
-    server.listen(config.port);
-})().catch(err => console.error('Error starting server', err.stack));
+    server.on('clientError', (err, socket) => {
+        socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+    });
 
-module.exports = server;
+    server.on('listening', () => {
+        const { host, port } = config;
+        console.log(`Server listening at http://${host}:${port} on process ${process.pid}`);
+    });
+
+    try {
+        server.listen(config.port);
+    } catch(err) {
+        console.error('Error starting server', err.stack);
+    }
+
+    return server;
+}
+
+if (require.main === module) {
+    (async () => {
+        // TODO: Create a database connection client for migrations, and a connection pool for the router.
+        const db = await pool.connect();
+        console.log('Created connection pool for PostgreSQL database');
+        const migrations = new Migrations(db);
+        await migrations.run();
+        await createCluster();
+    })().catch(err => {
+        console.error('Error starting server cluster', err.stack);
+    });
+}
